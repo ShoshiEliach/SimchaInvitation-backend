3a7c3af1255507ec435d662882caf3c0df502f90
shoshi <s0548566523@gmail.com>
2024-11-25 19:00:24 +0200

    25/11 first connect to data base

---
+++ Desktop/poject Tornado-systems/project-app/node-backend-side/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Hello from the Node.js backend!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
