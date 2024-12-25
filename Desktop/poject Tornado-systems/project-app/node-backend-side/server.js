2db905bd752ca4c0bd2716f070a2ffae9bc20f6c
shoshi <s0548566523@gmail.com>
2024-12-25 22:43:40 +0200

    25/12

--- Desktop/poject Tornado-systems/project-app/node-backend-side/server.js
+++ Desktop/poject Tornado-systems/project-app/node-backend-side/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const cors = require('cors');
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.json());
 
 app.use(cors());
 app.get('/', (req, res) => {
@@ -11,3 +15,51 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+
+///get address(SMS/mail) from the front-side:
+
+
+// נתיב לקבלת המספרים
+app.post("/api/phone-numbers", (req, res) => {
+    const { phoneNumbers } = req.body;
+
+    if (!phoneNumbers || !Array.isArray(phoneNumbers)) {
+        return res.status(400).json({ error: "Invalid phone numbers format" });
+    }
+
+    console.log("Received phone numbers:", phoneNumbers);
+
+    // כאן אפשר לשמור את המספרים למסד נתונים או לטפל בהם
+    res.status(200).json({ message: "Phone numbers received successfully" });
+});
+
+
+///SMTP2GO
+
+var nodemailer = require('nodemailer');
+
+var smtpTransport = nodemailer.createTransport({
+    host: "mail.smtp2go.com",
+    port: 2525, // 8025, 587 and 25 can also be used.
+    auth: {
+        user: "shoshana3631@gmail.com",
+        pass: "MrcWkDnaKoLXug48",
+    },
+});
+
+smtpTransport.sendMail({
+    from: "simchainvitation.com",
+    to: "s0548566523@gmail.com",
+    subject: "Your Subject",
+    text: "It is a test message",
+    html: "It is a test message"
+},
+    function (error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Message sent: " + response.message);
+        }
+    }
+);
